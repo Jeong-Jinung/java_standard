c9a5580fc5ba6168e67c91a6d50ba3c921e19c79
JeongJinUng <jinung.dev@gmail.com>
2022-01-11 23:33:23 +0900

    Initial commit

---
+++ src/ch02/Ex2_1.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class Ex2_1 {
+
+    public static void main(String[] args) {
+
+        //print() - 출력후에 줄바꿈을 안함
+        //println() - 출력 후에 줄바꿈을 합니다.
+        System.out.println(5+3); // 덧셈
+        System.out.println(5-3); // 뺄셈
+        System.out.println(5*3); // 곱셈
+        System.out.println(5/3); // 나눗셈
+        System.out.println(5/3.0); // 나눗셈
+    }
+}
